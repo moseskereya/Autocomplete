754df7dc2bbfcb32c7ca572a959974a7a4dd1cc4
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-30 19:50:56 +0300

    Updates

---
+++ precache-manifest.04a1556f47edd2bfcdafa794794c7be2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac906fc0decc5cd0cab94a159a2c4ca",
+    "url": "/Autocomplete/index.html"
+  },
+  {
+    "revision": "5f8288d2e2aeefa37f83",
+    "url": "/Autocomplete/static/css/main.0b097ffa.chunk.css"
+  },
+  {
+    "revision": "78fc312dd1382346190a",
+    "url": "/Autocomplete/static/js/2.eab113aa.chunk.js"
+  },
+  {
+    "revision": "5f8288d2e2aeefa37f83",
+    "url": "/Autocomplete/static/js/main.4ea0ff03.chunk.js"
+  },
+  {
+    "revision": "383c10e315c2e65a9b1f",
+    "url": "/Autocomplete/static/js/runtime-main.7dc7e016.js"
+  }
+]);
