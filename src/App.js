27871759d6f5f5a23afa29426d1c748263dee09a
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-30 19:46:15 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AutoCompletedText from './Components/Search';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AutoCompletedText />
+      </div>
+    );
+  }
+}
+
+export default App;
